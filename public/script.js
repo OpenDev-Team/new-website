a137c3b16160219410d391f4e5b0781556d02671
Glitch (hello-express) <none>
2020-08-14 18:56:54 +0000

    🕴🍁 Checkpoint ./views/index.html:9578983/3496 ./public/script.js:9578983/593

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
 mdc.autoInit()
+
+const listEl = document.querySelector('.mdc-drawer');
+const open = document.querySelector('#open');
+
+listEl.addEventListener('click', (event) => {
+  drawer.open = false;
+});
+
+listEl.addEventListener('click', (event) => {
+  drawer.open = true;
+});
