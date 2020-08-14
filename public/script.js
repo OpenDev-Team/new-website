b292d086b164a91a41c206277ee26538bd0dcd7d
Glitch (hello-express) <none>
2020-08-14 19:20:52 +0000

    🏟🎋 Checkpoint ./public/script.js:9578983/261

--- public/script.js
+++ public/script.js
@@ -17,3 +17,11 @@ function hidenav(){
     document.getElementById("drawer").style= "display: none;"
   }, 410);
 };
+
+function shownav(){
+  document.getElementById("drawer").style= "animation: 0.4s cubic-bezier(0.22, 1, 0.36, 1) 0s 1 slideInFromleft forwards;"
+  setTimeout(function() {
+    document.getElementById("drawer").style= "display: none;"
+  }, 410);
+};
+
