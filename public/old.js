87d61759dce8f383b23397d1878c2224e76051b2
Glitch (hello-express) <none>
2020-08-14 20:09:53 +0000

    ✂️🚖 Checkpoint ./views/index.html:9578983/40:19215956/2161 ./public/script.js:9578983/421:19215956/478 ./lethdev chat:19215956/10 ./public/style.css:9578983/1398:19215956/3261 ./lethdev chat.txt:19215956/103 ./views/old.html:19215956/2161 ./public/old.css:19215956/3161 ./public/old.js:19215956/479

--- public/old.js
+++ public/old.js
@@ -1,16 +1,5 @@
 mdc.autoInit()
 
-const listEl = document.querySelector('#drawer');
-const open = document.querySelector('#open');
-
-listEl.addEventListener('click', (event) => {
-  drawer.open = false;
-});
-
-listEl.addEventListener('click', (event) => {
-  drawer.open = true;
-});
-
 function hidenav(){
   document.getElementById("drawer").style= "animation: 0.4s cubic-bezier(0.22, 1, 0.36, 1) 0s 1 slideOutFromleft forwards;"
   setTimeout(function() {
@@ -19,9 +8,8 @@ function hidenav(){
 };
 
 function shownav(){
+  document.getElementById("drawer").style= "display: inline-block;"
   document.getElementById("drawer").style= "animation: 0.4s cubic-bezier(0.22, 1, 0.36, 1) 0s 1 slideInFromleft forwards;"
-  setTimeout(function() {
-    document.getElementById("drawer").style= "display: none;"
-  }, 410);
 };
+
 
